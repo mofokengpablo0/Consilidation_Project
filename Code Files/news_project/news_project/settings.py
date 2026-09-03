00c6fae1a735c7a80b5051df1f5db103054306eb
mofokengpablo0 <mofokengpablo0@gmail.com>
2026-09-03 21:31:20 +0200

    minor fixes

--- Code Files/news_project/news_project/settings.py
+++ Code Files/news_project/news_project/settings.py
@@ -74,7 +74,7 @@ elif DB_ENGINE == "sqlite":
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": BASE_DIR / "data" / "db.sqlite3",
         }
     }
 else:
